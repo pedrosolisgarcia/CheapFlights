bf3abd2cb06cdc346ebd2c30fced22b6f7e25597
Pedro Solís García <solisgpedro@gmail.com>
2018-05-06 14:13:55 +0200

    date-wrapped pseudo merged to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DateSelectorComponent } from './components/date-selector/date-selector.component';
+import { DateWrapperComponent } from './components/date-wrapper/date-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DateSelectorComponent
+    DateSelectorComponent,
+    DateWrapperComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/date-wrapper/date-wrapper.controller.ts
+++ src/app/components/date-wrapper/date-wrapper.controller.ts
@@ -1,15 +1,15 @@
-import moment from 'moment';
+import * as moment from 'moment';
 
 export default function DateWrapperController($scope) {
   'ngInject';
 
-  $scope.$watch('$ctrl.startDate', () => {
+  $scope.$watch('startDate', () => {
     if (moment(this.startDate) > moment(this.endDate)) {
       this.endDate = moment(this.startDate).add(2, 'd').toDate();
     }
   });
 
-  $scope.$watch('$ctrl.endDate', () => {
+  $scope.$watch('endDate', () => {
     if (moment(this.endDate) < moment(this.startDate)) {
       this.startDate = moment(this.endDate).subtract(2, 'd').toDate();
     }
