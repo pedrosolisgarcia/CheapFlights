8c5a7228b13cc77a51e88b322012b1a20f95656f
Pedro Solís García <solisgpedro@gmail.com>
2018-05-06 14:57:21 +0200

    flight list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,10 @@ import { AppComponent } from './app.component';
 import { DateSelectorComponent } from './components/date-selector/date-selector.component';
 import { DateWrapperComponent } from './components/date-wrapper/date-wrapper.component';
 import { AiportsService } from './services/airports.service';
+import { CheapFlightService } from './services/cheapflights.service';
 import { AirportSelectorComponent } from './components/airport-selector/airport-selector.component';
 import { FilterPipe } from './filter.pipe';
+import { FlightListComponent } from './components/flight-list/flight-list.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +18,15 @@ import { FilterPipe } from './filter.pipe';
     DateSelectorComponent,
     DateWrapperComponent,
     AirportSelectorComponent,
-    FilterPipe
+    FilterPipe,
+    FlightListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [AiportsService],
+  providers: [AiportsService, CheapFlightService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/flight-list/flight-list.component.ts
@@ -0,0 +1,75 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { DatePipe } from '@angular/common'
+import { CheapFlightService } from '../../services/cheapflights.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-flight-list',
+  templateUrl: './flight-list.component.html',
+  styleUrls: ['./flight-list.component.css']
+})
+export class FlightListComponent implements OnInit, OnDestroy {
+
+  paramsSubscription: Subscription;
+
+  departure: {iataCode: string, name: string};
+  destination: { iataCode: string, name: string};
+  startDate: any;
+  endDate: any;
+
+  flights = [
+    {
+    dateFrom: '',
+    dateTo: '',
+    currency: '',
+    price: 0
+  }]
+
+  constructor(private route: ActivatedRoute, private cheapFlightService: CheapFlightService, private datePipe: DatePipe) {}
+
+  ngOnInit() {
+    
+    this.departure = {
+      iataCode: this.route.snapshot.params['depCode'],
+      name: this.route.snapshot.params['depName']
+    }
+    this.destination = {
+      iataCode: this.route.snapshot.params['destCode'],
+      name: this.route.snapshot.params['destName']
+    }
+    this.startDate = this.route.snapshot.params['startDate'];
+    this.endDate = this.route.snapshot.params['endDate'];
+
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.departure.iataCode = params['depCode'];
+        this.departure.name = params['depName'];
+        this.destination.iataCode = params['destCode'];
+        this.destination.name = params['destName'];
+        this.startDate = params['startDate'];
+        this.endDate = params['endDate'];
+      }
+    );
+    this.onShowFlights(this.departure, this.destination, this.startDate, this.endDate);
+  }
+
+  formatDate(date: any) {
+    return  this.datePipe.transform(date, 'yyyy-MM-dd');
+  }
+
+  onShowFlights(departure: any, destination: any, startDate: any, endDate: any) {
+
+    this.cheapFlightService.getFlights(departure, destination, this.formatDate(startDate), this.formatDate(endDate)).subscribe(
+      (flights: any) => {
+        this.flights = flights;
+        console.log(this.flights);
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
+}
